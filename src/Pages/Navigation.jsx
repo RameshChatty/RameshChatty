2ba6322469912b4735b4a5ead089fef4355e6080
Chatty Ramesh <chattyramesh412@gmail.com>
2025-04-05 15:05:47 +0530

    minor changes

--- src/Pages/Navigation.jsx
+++ src/Pages/Navigation.jsx
@@ -54,10 +54,8 @@ export default function Navigation() {
                     to={item.href}
                     aria-current={item.current ? "page" : undefined}
                     className={classNames(
-                      item.current
-                        ? "bg-gray-900 text-white"
-                        : "text-gray-300 hover:bg-gray-700 hover:text-white",
-                      "rounded-md px-3 py-2 text-sm font-medium"
+                      "text-gray-300 hover:bg-gray-700 hover:text-white hover:font-extrabold ",
+                      "rounded-md px-3 py-2 text-sm"
                     )}
                   >
                     {item.name}
@@ -67,15 +65,6 @@ export default function Navigation() {
             </div>
           </div>
           <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
-            <button
-              type="button"
-              className="relative rounded-full bg-gray-800 p-1 text-gray-400 hover:text-white focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800 focus:outline-hidden"
-            >
-              <span className="absolute -inset-1.5" />
-              <span className="sr-only">View notifications</span>
-              <BellIcon aria-hidden="true" className="size-6" />
-            </button>
-
             {/* Profile dropdown */}
             <Menu as="div" className="relative ml-3">
               <div>
